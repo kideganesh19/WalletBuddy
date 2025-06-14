8cd40a1b58b83a9299d3a30bed0055361dad968c
kideganesh19 <ganeshkide123@gmail.com>
2025-06-14 14:18:15 +0530

    query to find by id

--- userservice/src/main/java/com/walletBuddy/userservice/repository/UserRepository.java
+++ userservice/src/main/java/com/walletBuddy/userservice/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.walletBuddy.userservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.walletBuddy.userservice.models.entity.UserInfo;
 
 public interface UserRepository extends JpaRepository<UserInfo, String>{
+	
+	Optional<UserInfo> findByEmailId(String emailId);
 
 }
--- userservice/src/main/java/com/walletBuddy/userservice/service/UserService.java
+++ userservice/src/main/java/com/walletBuddy/userservice/service/UserService.java
@@ -1,21 +1,54 @@
 package com.walletBuddy.userservice.service;
 
+import java.util.Collections;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.walletBuddy.userservice.exception.DuplicateUserException;
 import com.walletBuddy.userservice.models.entity.UserInfo;
 import com.walletBuddy.userservice.models.request.CreateUserRequest;
 import com.walletBuddy.userservice.repository.UserRepository;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService{
 	
 	
 	@Autowired
 	UserRepository userRepository;
 	
+	@Autowired
+    PasswordEncoder encoder;
+	
 	public UserInfo createNewUser(CreateUserRequest request){
-		UserInfo userEntity = userRepository.save(request.toUserInfo());
+		UserInfo userInfo = request.toUserInfo();
+		Optional<UserInfo> user = userRepository.findByEmailId(userInfo.getEmailId());
+		if(user.isPresent()) {
+			throw new DuplicateUserException();
+		}
+		
+		userInfo.setPassword(encoder.encode(userInfo.getPasswordRaw()));
+		UserInfo userEntity = userRepository.save(userInfo);
 		return userEntity;
 	}
+	
+	public static UserInfo anonymous(){
+		UserInfo userInfo = new UserInfo();
+		userInfo.setName("ANONYMOUS");
+		userInfo.setId(null);
+		userInfo.setPassword("ANONYMOUS");
+		userInfo.setRoles(Collections.singleton("ROLE_ANONYMOUS"));
+		return userInfo;
+    }
+
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<UserInfo> userOpt = userRepository.findByEmailId(username);
+        return userOpt.orElseGet(UserService::anonymous);
+    }
 }
