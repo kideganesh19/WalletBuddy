b29782e8bf4460458207e27737d9075260cca3df
kideganesh19 <ganeshkide123@gmail.com>
2025-06-14 20:08:32 +0530

    adding mapping annotation

--- walletservice/src/main/java/com/walletBuddy/walletservice/controller/WalletController.java
+++ walletservice/src/main/java/com/walletBuddy/walletservice/controller/WalletController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +38,7 @@ public class WalletController {
 		
 	}
 	
-	
+	@PostMapping(value="/create", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<WalletTransaction> createTransaction( @Valid @RequestBody WalletRequest request){
 		
 		var walletTxnEntity = walletTransactionService.createTxn(request);
