da3732ff7314817e529270978ebe3bd3df165dd0
kideganesh19 <ganeshkide123@gmail.com>
2025-06-14 20:05:37 +0530

    adding post api controller

--- walletservice/src/main/java/com/walletBuddy/walletservice/controller/WalletController.java
+++ walletservice/src/main/java/com/walletBuddy/walletservice/controller/WalletController.java
@@ -6,11 +6,17 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.walletBuddy.walletservice.models.entity.Wallet;
+import com.walletBuddy.walletservice.models.entity.WalletTransaction;
+import com.walletBuddy.walletservice.models.request.WalletRequest;
 import com.walletBuddy.walletservice.service.WalletService;
+import com.walletBuddy.walletservice.service.WalletTransactionService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/v1/wallet")
@@ -19,12 +25,23 @@ public class WalletController {
 	@Autowired
 	WalletService walletService;
 	
+	@Autowired
+	WalletTransactionService walletTransactionService;
+	
 	
 	@GetMapping(value="/{walletId}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Wallet> getWalletById(@PathVariable String walletId){
 		
 		var walletEntity = walletService.getWalletById(walletId);
 		return new ResponseEntity<>(walletEntity,HttpStatus.OK);
+		
+	}
+	
+	
+	public ResponseEntity<WalletTransaction> createTransaction( @Valid @RequestBody WalletRequest request){
+		
+		var walletTxnEntity = walletTransactionService.createTxn(request);
+		return new ResponseEntity<>(walletTxnEntity, HttpStatus.OK);
 		
 	}
 	
